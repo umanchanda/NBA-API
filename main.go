d26f3563f74560c9075832653ef131510410e99f
Uday Manchanda <uday.manchanda14@gmail.com>
2020-04-21 15:19:38 -0400

    got rid of csv to json conversion

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
@@ -82,79 +78,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/index.html")
 }
 
-func readCSVFile(filename string) string {
-	NBACSVFile, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-	}
-	reader := csv.NewReader(bufio.NewReader(NBACSVFile))
-	var players []NBAPlayer
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			fmt.Println(err)
-		}
-		players = append(players, NBAPlayer{
-			Name:     line[0],
-			Height:   line[1],
-			Weight:   line[2],
-			Team:     line[3],
-			Age:      line[4],
-			Salary:   line[5],
-			Points:   line[6],
-			Blocks:   line[7],
-			Steals:   line[8],
-			Assists:  line[9],
-			Rebounds: line[10],
-			FT:       line[11],
-			FTA:      line[12],
-			FG3:      line[13],
-			FG3A:     line[14],
-			FG:       line[15],
-			FGA:      line[16],
-			MP:       line[17],
-			G:        line[18],
-			PER:      line[19],
-			OWS:      line[20],
-			DWS:      line[21],
-			WS:       line[22],
-			WS48:     line[23],
-			USG:      line[24],
-			BPM:      line[25],
-			VORP:     line[26],
-		})
-	}
-
-	playersJSON, _ := json.Marshal(players)
-	return string(playersJSON)
-}
-
-func searchPlayer(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		http.ServeFile(w, r, "templates/search.html")
-	case "POST":
-		player := r.FormValue("player")
-		resp, err := http.Get("localhost:8000/players")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		var players []NBAPlayer
-		err = json.Unmarshal(body, &players)
-		fmt.Fprintf(w, string(player))
-
-	}
-}
-
 func getHTML(month string, day string, year string) []byte {
 	var url = baseURL + "/boxscores/?month=" + month + "&day=" + day + "&year=" + year
 	resp, err := http.Get(url)
@@ -215,17 +138,10 @@ func extractGameSummary(month string, day string, year string) string {
 }
 
 func main() {
-
-	playerData := readCSVFile("nbastats2018-2019.csv")
-
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", index)
-	r.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, playerData)
-	})
-	r.HandleFunc("/boxscores", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hey")
-	})
+	// r.HandleFunc("/searchPlayer", searchPlayer)
 	r.HandleFunc("/boxscore/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		month := vars["month"]
