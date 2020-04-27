86334d6f45889a9fa6060b3e72bdc3dbeb9e4342
Uday Manchanda <uday.manchanda14@gmail.com>
2020-04-26 22:14:57 -0400

    removed map and url, add new route for search, and imported database package

--- main.go
+++ main.go
@@ -6,55 +6,25 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	
+
+	"github.com/umanchanda/NBA-API/database"
+	"github.com/umanchanda/NBA-API/playertotals"
 	"github.com/umanchanda/NBA-API/teamboxscore"
 	"github.com/umanchanda/NBA-API/teamtotals"
-	"github.com/umanchanda/NBA-API/playertotals"
 )
 
-const baseURL = "https://www.basketball-reference.com"
-
-var teamCodes = map[string]string{
-	"Atlanta":       "ATL",
-	"Boston":        "BOS",
-	"Brooklyn":      "BKN",
-	"Charlotte":     "CHO",
-	"Chicago":       "CHI",
-	"Cleveland":     "CLE",
-	"Dallas":        "DAL",
-	"Denver":        "DEN",
-	"Detroit":       "DET",
-	"Golden State":  "GSW",
-	"Houston":       "HOU",
-	"Indiana":       "IND",
-	"LA Lakers":     "LAL",
-	"LA Clippers":   "LAC",
-	"Memphis":       "MEM",
-	"Miami":         "MIA",
-	"Milwaukee":     "MIL",
-	"Minnesota":     "MIN",
-	"New Orleans":   "NOP",
-	"New York":      "NYK",
-	"Oklahoma City": "OKC",
-	"Orlando":       "ORL",
-	"Philadelphia":  "PHI",
-	"Phoenix":       "PHO",
-	"Portland":      "POR",
-	"Sacramento":    "SAC",
-	"San Antonio":   "SAS",
-	"Toronto":       "TOR",
-	"Utah":          "UTA",
-	"Washington":    "WAS",
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "templates/index.html")
-}
-
 func main() {
+
+	database.DatabaseFunctions()
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", index)
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/index.html")
+	})
+	r.HandleFunc("/searchPlayer", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/search.html")
+	})
 	r.HandleFunc("/boxscore/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		month := vars["month"]
